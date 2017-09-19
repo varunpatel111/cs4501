812e376e5f49e359d54b581ce680e0b2d40ef5ce
Ben Charles <benkcharles@gmail.com>
2017-09-19 15:02:56 -0400

    Add attributes to models

--- app/leaseMe/marketplace/models.py
+++ app/leaseMe/marketplace/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+import datetime
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
+class CustomUser(User):
+
 
 
 class Listing(models.Model):
+	address = models.CharField(max_length=50, required=True)
+	num_bedrooms = models.IntegerField(required=True)
+	num_bathrooms = models.IntegerField(required=True)
+	price = models.IntegerField(required=True)
+	start_date = models.DateField(required=True)
+    end_date = models.DateField(required=True)
+    description = models.TextField(max_length=300)
+    sold = models.BooleanField(default=False)
 
