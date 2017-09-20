433efad6da5d3cb5b25b2f03c2a4cc088d22322d
Ben Charles <benkcharles@gmail.com>
2017-09-19 20:29:05 -0400

    Finish models

--- app/leaseMe/marketplace/models.py
+++ app/leaseMe/marketplace/models.py
@@ -4,17 +4,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class CustomUser(User):
-
+class CustomUser(models.Model):
+	email = models.EmailField()
+	password = models.CharField(max_length=50)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	#just make a simple user for now
 
 
 class Listing(models.Model):
-	address = models.CharField(max_length=50, required=True)
-	num_bedrooms = models.IntegerField(required=True)
-	num_bathrooms = models.IntegerField(required=True)
-	price = models.IntegerField(required=True)
-	start_date = models.DateField(required=True)
-    end_date = models.DateField(required=True)
-    description = models.TextField(max_length=300)
-    sold = models.BooleanField(default=False)
+	address = models.CharField(max_length=50)
+	num_bedrooms = models.IntegerField()
+	num_bathrooms = models.IntegerField()
+	price = models.IntegerField()
+	start_date = models.DateField()
+	end_date = models.DateField()
+	description = models.TextField(max_length=300, blank=True)
+	sold = models.BooleanField(default=False)
+	user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
