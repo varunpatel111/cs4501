469c7a148109e554dd0f341c37ae9bcd35136c84
varunpatel111 <vp2xb@virginia.edu>
2017-09-20 11:50:03 -0400

    added more api methods

--- app/leaseMe/marketplace/serializers.py
+++ app/leaseMe/marketplace/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from .models import CustomUser
 from .models import Listing
+import json
 
-class CustomUserSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = CustomUser
-		fields = '__all__'
+class CustomUserSerializer():
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
 
 class ListingSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Listing
 		fields = '__all__'
-
--- app/leaseMe/marketplace/views.py
+++ app/leaseMe/marketplace/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from django.http import HttpResponse
 import json
 from django.core import serializers
-
+from django.views.decorators.csrf import csrf_exempt
 #class CustomUserViewSet(viewsets.ModelViewSet):
 #    queryset = CustomUser.objects.all()
 #    serializer_class = CustomUserSerializer
@@ -17,3 +17,38 @@ def all_users(request):
 	queryset = CustomUser.objects.all()
 	r = serializers.serialize('json', queryset)
 	return HttpResponse(r)
+
+def get_user(request, user):
+	queryset = CustomUser.objects.all()
+	r = list(queryset)
+	user = int(user)
+	if r[user]:
+		return HttpResponse(json.dumps(r[user], default=lambda o: o.__dict__,
+            sort_keys=True, indent=4))
+	else:
+		return None
+
+
+@csrf_exempt
+def users_create(request):
+	if request.method == "POST":
+		return HttpResponse("got it")
+	else:
+		return HttpResponse("got it")
+
+
+
+def all_listings(request):
+	queryset = Listing.objects.all()
+	r = serializers.serialize('json', queryset)
+	return HttpResponse(r)
+
+def get_listing(request, listing):
+	queryset = Listing.objects.all()
+	r = list(queryset)
+	listing = int(listing)
+	if r[listing]:
+		return HttpResponse(json.dumps(r[listing], default=lambda o: o.__dict__,
+            sort_keys=True, indent=4))
+	else:
+		return None
