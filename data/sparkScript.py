b99522a1527f5f8a61081e5c6c90e32d00b2f6dc
varunpatel111 <vp2xb@virginia.edu>
2017-12-02 14:51:22 -0500

    Finished spark script

--- data/sparkScript.py
+++ data/sparkScript.py
@@ -5,15 +5,17 @@ sc = SparkContext("spark://spark-master:7077", "RecommendedItems")
 data = sc.textFile("/tmp/data/accessLog.txt", 2)     # each worker loads a piece of the data file
 
 pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
-                                                  # and then reduce all the values by adding them together
+pages = pairs.map(lambda pair: (pair[0], pair[1])).distinct().join(pairs.map(lambda pair: (pair[0], pair[1])).distinct()).distinct()
+output = pages.map(lambda x: (x[1], x[0])).groupByKey()
+output = output.map(lambda x: (x[0], len(x[1]))).filter(lambda x: list(x[0])[0] != list(x[0])[1]).filter(lambda x: x[1] > 2).collect()
 
-output = count.collect()
 
-                         # bring the data back to the master node so we can print it out
-for page_id, count in output:
-    print ("page_id %s count %d" % (page_id, count))
-print ("Popular items done")
+for x in output:
+    print(x[0], (x[1]))
+
+
+
+
+print ("Recommended Items items done")
 
 sc.stop()
